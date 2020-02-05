d7b9507a91c47d6be006d4daf5a009223b661856
sienmich <sienmich@gmail.com>
2020-02-05 12:49:54 +0100

    del images.cc

--- images.h
+++ images.h
@@ -28,15 +28,26 @@ using Blend = Base_image<Fraction>;
 namespace Detail {
     /// Alternate returns true iff. x is in (let i = interval)
     /// ... or [-3i, -2i) or [-i, 0) or [i, 2i) or [3i, 4i) or ... .
-    bool alternate(double x, double interval);
+    constexpr bool alternate(double x, double interval) {
+        x += 2 * interval * (x < 0 ? (ceil(-x / interval / 2.0)) : 0);
+        return (int) (x / interval) % 2;
+    }
 
     /// Returns a function that scales every point in x axis by v.first and
     /// in the y axis by v.second.
-    Base_image<Point> scale(const Vector &v);
+    inline Base_image<Point> scale(const Vector &v) {
+        return [=](const Point p) {
+            return (Point(p.first / v.first, p.second / v.second, p.is_polar));
+        };
+    }
 
     /// Returns a function that moves every point in x axis by v.first and
     /// in the y axis by v.second.
-    Base_image<Point> translate(const Vector &v);
+    inline Base_image<Point> translate(const Vector &v) {
+        return [=](const Point p) {
+            return (Point(p.first - v.first, p.second - v.second, p.is_polar));
+        };
+    }
 }
 
 /// Returns a function that for every point returns t.
@@ -115,20 +126,29 @@ Base_image<T> vertical_stripe(double d, const T &this_way, const T &that_way) {
     };
 }
 
-/// Returns an Image that for points inside region is like this_way, for the
-/// rest is like that_way.
-Image cond(const Region &region, const Image &this_way, const Image &that_way);
-
 /// Returns an Image that is a result of blending (with parameter blend)
 /// this_way and that_way.
-Image lerp(const Blend &blend, const Image &this_way, const Image &that_way);
+inline Image lerp(const Blend &blend, const Image &this_way, const Image &that_way) {
+    return [=](const Point p) {
+        return this_way(p).weighted_mean(that_way(p), blend(p));
+    };
+}
+
+/// Returns an Image that for points inside region is like this_way, for the
+/// rest is like that_way.
+inline Image cond(const Region &region, const Image &this_way, const Image &that_way) {
+    return lerp(region, this_way, that_way);
+}
 
 /// Returns an Image that is a result of blending (with parameter blend)
 /// image and black color.
-Image darken(const Image &image, const Blend &blend);
+inline Image darken(const Image &image, const Blend &blend) {
+    return lerp(blend, image, constant(Colors::black));
+}
 
 /// Returns an Image that is a result of blending (with parameter blend)
 /// image and white color.
-Image lighten(const Image &image, const Blend &blend);
-
+inline Image lighten(const Image &image, const Blend &blend) {
+    return lerp(blend, image, constant(Colors::white));
+}
 #endif //IMAGES_H
